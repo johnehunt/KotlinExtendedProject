99572171d2e0f24fb7c13b7e9dd39060510d378c
John Hunt <john.hunt10@gmail.com>
2021-03-18 12:42:42 +0000

    Updated gradle

--- ConferenceOrganizerApp/app/src/main/java/com/jjh/android/organizer/ui/conference/TrackTabFragment.kt
+++ ConferenceOrganizerApp/app/src/main/java/com/jjh/android/organizer/ui/conference/TrackTabFragment.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jjh.android.organizer.R
 import com.jjh.android.organizer.model.TrackWithSessions
-import kotlinx.android.synthetic.main.tab_fragment.*
 
 class TrackTabFragment(private val trackWithSessions: TrackWithSessions): Fragment() {
 
@@ -21,23 +21,26 @@ class TrackTabFragment(private val trackWithSessions: TrackWithSessions): Fragme
         Log.d(TAG, "init{}")
     }
 
+    private lateinit var trackRecyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         Log.d(TAG, "onCreateView()")
-        return inflater.inflate(R.layout.tab_fragment, container, false)
+        val view =  inflater.inflate(R.layout.tab_fragment, container, false)
+        trackRecyclerView = view.findViewById(R.id.trackRecyclerView)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "onViewCreated()")
 
-        trackRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-            Log.d(TAG, "onViewCreated - sessions: ${trackWithSessions.sessions.toString()}")
-            adapter = SessionRowAdapter(context, trackWithSessions.sessions)
-        }
+        trackRecyclerView.layoutManager = LinearLayoutManager(context)
+        Log.d(TAG, "onViewCreated - sessions: ${trackWithSessions.sessions.toString()}")
+        trackRecyclerView.adapter = SessionRowAdapter(requireActivity(),
+                                                      trackWithSessions.sessions)
 
     }
 
